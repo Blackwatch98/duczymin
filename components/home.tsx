4d11b979a38c1a9b08a4bfdf9b3a120a99d5ccfc
Daniel Duczymiński <danielduczyminski@gmail.com>
2023-04-19 00:09:51 +0200

    navbar experiments

--- components/home.tsx
+++ components/home.tsx
@@ -10,7 +10,7 @@ export interface CarouselImage {
   description?: string,
 }
 
-export type MyImage = {
+export interface MyImage {
   src: string,
   alt: string,
   ref: string,
@@ -18,7 +18,7 @@ export type MyImage = {
   description: string
 }
 
-const myImages: MyImage[] = [
+const homeProductsImages: MyImage[] = [
   {src: 'okna-pcv.jpg', alt:'okna-pcv', ref: "/okna", title:'Okna PCV', description:'Na razie brak. To jest teki przykładowy tekst dla jaj żeby sprawdzić czy coś tu w ogóle działa :)'},
   {src: 'brama.jpg', alt:'brama', ref: "/bramy", title:'Bramy garażowe', description:'Na razie brak.'},
   {src: 'rolety-zew.jpg', alt:'rolety-zew', ref: "/rolety", title:'Rolety zewnętrzne', description:'Na razie brak.'},
@@ -29,26 +29,27 @@ const myImages: MyImage[] = [
   {src: 'parapet-wewnętrzny.jpg', alt:'parapety', ref: "/parapety", title:'Parapety', description:'Na razie brak.'},
 ]
 
-const myImages2: MyImage[] = [
+const companyProductsImages: MyImage[] = [
   {src: 'okna-pcv.jpg', alt:'bramy-przemyslowe', ref: "/bramy-przemyslowe", title:'Bramy przemysłowe', description:'Na razie brak. To jest teki przykładowy tekst dla jaj żeby sprawdzić czy coś tu w ogóle działa :)'},
   {src: 'brama.jpg', alt:'bramy-rolowane', ref: "/bramy-rolowane", title:'Bramy rolowane', description:'Na razie brak.'},
   {src: 'rolety-zew.jpg', alt:'kraty-rolowane', ref: "/kraty-rolowane", title:'Kraty zewnętrzne', description:'Na razie brak.'},
   {src: 'drzwi.jpg', alt:'stolarka-aluminiowa', ref: "/stolarka-aluminiowa", title:'Stolarka aluminiowa', description:'Na razie brak.'}
 ]
 
-const images : CarouselImage[] = [{src: "/home.png"},
-                                  {src: "/brama.jpg"},
-                                  {src: "/okna-marzen.jpg"}]
+const carouselImages : CarouselImage[] = [
+  {src: "/home.png"},
+  {src: "/brama.jpg"},
+  {src: "/promotions/promotion.jpg"}
+]
 
-function MyHome() {
+export default function HomePage() {
   const [currentIndex, setCurrentIndex] = useState(1);
 
   const moveNext = () => {
-    if(currentIndex !== images.length) {
+    if(currentIndex !== carouselImages.length) {
       setCurrentIndex(currentIndex + 1);
     }
-    else if(currentIndex === images.length) {
-      console.log("tutaj2");
+    else if(currentIndex === carouselImages.length) {
       setCurrentIndex(1);
     }
   }
@@ -58,7 +59,7 @@ function MyHome() {
       setCurrentIndex(currentIndex - 1);
     }
     else if(currentIndex === 1) {
-      setCurrentIndex(images.length);
+      setCurrentIndex(carouselImages.length);
     }
   }
 
@@ -75,14 +76,14 @@ function MyHome() {
 
   return (
     <>
-      <div className={styles.div}>
+      <div className={styles.carouselDiv}>
         <section aria-label="News">
           <div className={styles.carousel} data-carousel>
             <button onClick={() => movePrev()} className={`${styles["carousel-button"]} ${styles.prev}`}>&#8656;</button>
             <button onClick={() => moveNext()} className={`${styles["carousel-button"]} ${styles.next}`}>&#8658;</button>
             <ul>
               {
-                images.map((image, imageIndex) => (
+                carouselImages.map((image, imageIndex) => (
                 <li className={currentIndex === imageIndex + 1 ? styles.slideActive : styles.slide}>
                   <Image width={1000} height={100} className={styles.slideImage} src={image.src} alt='home'/>
                 </li>))
@@ -99,9 +100,9 @@ function MyHome() {
           </div>
         </section>
         <h1 className={styles.h1}>Produkty dla domów</h1>
-        <Gallery images={myImages}/>
+        <Gallery images={homeProductsImages}/>
         <h1 className={styles.h1}>Produkty dla firm</h1>
-        <Gallery images={myImages2}/>
+        <Gallery images={companyProductsImages}/>
         <h1 className={styles.h1}>Obejrzyj nasze realizacje</h1>
         <h1 className={styles.h1}>Znajdź nas</h1>
         <Map/>
@@ -110,6 +111,3 @@ function MyHome() {
     </>
   );
 }
-
-
-export default MyHome;
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,22 +1,35 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useState, useRef, useEffect, UIEvent } from 'react'
 import Link from 'next/link'
 import styles from '../styles/Navbar.module.css'
-import img from '../public/duczymin-logo.png'
+import img from '../public/duczymin-logo-biale.png'
 import Image from 'next/image'
-
 
 export default function NavBar() {
     const [isNavbarExpanded, setIsNavbarExpanded] = useState(true);
 
+    const [isNavbarSticky, setisNavbarSticky] = useState(false);
+
+    const scrollNavbar = () => {
+        if(window.scrollY > 0) {
+            setisNavbarSticky(true);
+        } else {
+            setisNavbarSticky(false);
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener("scroll", scrollNavbar); 
+        return () => window.removeEventListener("scroll", scrollNavbar);
+    });
+
     let isActiveClass = isNavbarExpanded ? `${styles["navbar-links"]} ${styles.magic}` : styles["navbar-links"]
+    let isStickyNavClass = isNavbarSticky ? `${styles["navbar"]} ${styles["sticky"]}`: styles["navbar"]
 
     return (
-        <nav className={styles.navbar}>
-            <div className={styles["brand-title"]}>
-                <Link className={styles.logo} href="/">
-                    <Image src={img} alt='logo'/>
-                </Link>
-            </div>
+        <nav className={isStickyNavClass} onScroll={scrollNavbar}>
+            <Link className={styles.logo} href="/">
+                <Image src={img} alt='logo'/>
+            </Link>
             <a href='#' className={styles["toggle-button"]} onClick={() => setIsNavbarExpanded(!isNavbarExpanded)}>
                 <span className={styles.bar}></span>
                 <span className={styles.bar}></span>
@@ -24,51 +37,36 @@ export default function NavBar() {
             </a>
             <div className={`${isActiveClass}`}>
                 <ul className={styles.ul} onClick={() => setIsNavbarExpanded(false)}>
+                    <div className={styles["dropdown"]}>
+                        <li>
+                            <Link className={styles.link} href="/">
+                                Produkty
+                            </Link>
+                        </li>
+                        <div className={styles["dropdown-menu"]}>
+                            Dropdown Content
+                        </div>
+                    </div>
                     <li>
                         <Link className={styles.link} href="/">
-                            Okna PCV
+                            O firmie
                         </Link>
                     </li>
                     <li>
                         <Link className={styles.link} href="/">
-                            Bramy garażowe
+                            Nasze realizacje
                         </Link>
                     </li>
                     <li>
                         <Link className={styles.link} href="/">
-                            Rolety
-                        </Link>
-                    </li>
-                    <li>
-                        <Link className={styles.link} href="/">
-                            Żaluzje fasadowe
-                        </Link>
-                    </li>
-                    <li>
-                        <Link className={styles.link} href="/">
-                            Drzwi
-                        </Link>
-                    </li>
-                    <li>
-                        <Link className={styles.link} href="/">
-                            Rolety wewnętrzne
-                        </Link>
-                    </li>
-                    <li>
-                        <Link className={styles.link} href="/">
-                            Automatyka
-                        </Link>
-                    </li>
-                    <li>
-                        <Link className={styles.link} href="/">
-                            Parapety
+                            Wirtualny salon
                         </Link>
                     </li>
                     <li>
                         <Link className={styles.link} href="/">
                             Kontakt
                         </Link>
-                    </li>   
+                    </li>
                 </ul>
             </div>
         </nav>
