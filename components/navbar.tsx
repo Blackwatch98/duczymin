5037731b45e5a23e3890405d2e29342d52133ea1
Daniel <daniel.duczyminski@rossmann.com.pl>
2024-04-21 18:26:05 +0200

    navbar mobile dropdown first fix

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,38 +1,25 @@
-import React, { useState, useRef, useEffect, UIEvent } from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
 import { useRouter } from 'next/router';
 import styles from '../styles/Navbar.module.css'
-//import img from '../public/duczymin-logo-biale.png'
 import img from '../public/logo_duczymin.png'
 import Image from 'next/image'
 
 export default function NavBar() {
-    const [isNavbarExpanded, setIsNavbarExpanded] = useState(true);
-    const [isNavbarSticky, setisNavbarSticky] = useState(false);
+    const [isNavbarExpanded, setIsNavbarExpanded] = useState(false);
     const [isProductsClicked, setIsProductsClicked] = useState(false);
     const router = useRouter();
 
-    const scrollNavbar = () => {
-        if(window.scrollY > 0) {
-            setisNavbarSticky(true);
-        } else {
-            setisNavbarSticky(false);
-        }
-    }
-
-    useEffect(() => {
-        window.addEventListener("scroll", scrollNavbar); 
-        return () => window.removeEventListener("scroll", scrollNavbar);
-    });
-
     const isActiveClass = isNavbarExpanded
-    ? `${styles['navbar-links']} ${styles.magic}`
-    : styles['navbar-links'];
-
-    //let isActiveClass = isNavbarExpanded ? `${styles["navbar-links"]} ${styles.magic}` : styles["navbar-links"]
+    ? `${styles['navbar-links-container']} ${styles['navbar-mobile']}`
+    : styles['navbar-links-container'];
 
     const isLinkActive = (href: any) => router.pathname === href;
 
+    const toggleProductsDropdown = () => {
+        setIsProductsClicked(!isProductsClicked);
+    };
+    
     const isProductLinkActive = isLinkActive('/produkty/okna-pcv') || isLinkActive('/produkty/bramy-garazowe') ||
                                 isLinkActive('/produkty/rolety-zewnetrzne') || isLinkActive('/produkty/drzwi-zewnetrzne') ||
                                 isLinkActive('/produkty/zaluzje-fasadowe') || isLinkActive('/produkty/oslony-wewnetrzne') ||
@@ -42,7 +29,7 @@ export default function NavBar() {
 
     return (
         <nav className={styles.navbar}>
-            <Link className={styles.logo} href="/">
+            <Link className={styles['brand-logo']} href="/">
                 <Image src={img} alt='logo'/>
             </Link>
             <a href='#' className={styles["toggle-button"]} onClick={() => setIsNavbarExpanded(!isNavbarExpanded)}>
@@ -51,9 +38,12 @@ export default function NavBar() {
                 <span className={styles.bar}></span>
             </a>
             <div className={`${isActiveClass}`}>
-                <ul className={styles.ul} onClick={() => setIsNavbarExpanded(false)}>
-                    <div className={styles.dropdown} onMouseLeave={() => setIsProductsClicked(false)}>
-                        <li className={`${styles.link} ${isProductLinkActive ? styles.active : ''}`} onMouseOver={() => setIsProductsClicked(true)}>
+                <ul className={styles.ul}>
+                    <div className={styles.dropdown}
+                        onMouseLeave={() => window.innerWidth > 850 ? setIsProductsClicked(false) : null}>
+                        <li className={`${styles.link} ${isProductLinkActive ? styles.active : ''}`}
+                            onClick={() => window.innerWidth <= 850 ? toggleProductsDropdown() : null}
+                            onMouseOver={() => window.innerWidth > 850 ? setIsProductsClicked(true) : null}>
                             Produkty
                         </li>
                         <div className={`${styles["dropdown-menu"]} ${isProductsClicked ? styles.active : styles.inactive}`}>
@@ -81,24 +71,25 @@ export default function NavBar() {
                             </div>
                         </div>
                     </div>
-                    <li>
+                    <li onClick={() => setIsNavbarExpanded(false)}>
                         <Link className={`${styles.link} ${isLinkActive('/o-firmie') ? styles.active : ''}`} href="/o-firmie">
                             O firmie
                         </Link>
                     </li>
+                    {/*
                     <li>
-                        {/*
                         <Link className={styles.link} href="/realizacje">
                             Nasze realizacje
                         </Link>
-                        */}
+                        
                     </li>
-                    <li>
+                    */}
+                    <li onClick={() => setIsNavbarExpanded(false)}>
                         <Link className={`${styles.link} ${isLinkActive('/salon') ? styles.active : ''}`} href="/salon">
                             Wirtualny salon
                         </Link>
                     </li>
-                    <li>
+                    <li onClick={() => setIsNavbarExpanded(false)}>
                         <Link className={`${styles.link} ${isLinkActive('/kontakt') ? styles.active : ''}`} href="/kontakt">
                             Kontakt
                         </Link>
